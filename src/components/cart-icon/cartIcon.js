60da64ef7968163169b2693ce14f754fe9464fa2
alejandro <alejandrodanielaquino@yahoo.com.ar>
2021-02-03 23:45:45 -0300

    add cart dropdown functionality. add cart item count functionality

--- src/components/cart-icon/cartIcon.js
+++ src/components/cart-icon/cartIcon.js
@@ -3,11 +3,11 @@ import { ReactComponent as ShoppingIcon } from "../../assets/shopping-bag.svg";
 import "./cart-icon.scss";
 import { connect } from "react-redux";
 import { toggleCartHidden } from "../../redux/cart/cart.actions";
-const cartIcon = ({ toggleCartHidden }) => {
+const cartIcon = ({ toggleCartHidden, itemCount }) => {
   return (
     <div className="cart-icon" onClick={toggleCartHidden}>
       <ShoppingIcon className="shopping-icon" />
-      <span className="item-count">0</span>
+      <span className="item-count">{itemCount}</span>
     </div>
   );
 };
@@ -16,4 +16,11 @@ const mapDispatchToProps = dispatch => ({
   toggleCartHidden: () => dispatch(toggleCartHidden())
 });
 
-export default connect(null, mapDispatchToProps)(cartIcon);
+const mapStateToProps = ({ cart: { cartItems } }) => ({
+  itemCount: cartItems.reduce(
+    (accumulatedQuantity, cartItem) => accumulatedQuantity + cartItem.quantity,
+    0
+  )
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(cartIcon);
